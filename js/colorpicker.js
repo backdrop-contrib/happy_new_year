9a6f4bee9e6e56b82a5e4672dfff1881378506bb
Andrey Ivnitsky <info@itcrowd72.ru>
2018-11-11 12:13:13 +0500

    Issue #3012876 by itcrowd72: Code standard

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -5,8 +5,8 @@
   Drupal.behaviors.colorPicker = {
     attach: function (context, settings) {
 
-      $(document).ready(function() {
-        $("#color-picker").farbtastic("#edit-happy-new-year-snowcolor");
+      $(document).ready(function () {
+        $('#color-picker').farbtastic('#edit-happy-new-year-snowcolor');
       });
 
     }
--- js/garland.js
+++ js/garland.js
@@ -8,10 +8,12 @@
       var num = 0;
       function garland() {
         $('#garland').css('backgroundPosition', '0 -' + num + 'px');
-        if (num > 72)
+        if (num > 72) {
           num = 36;
-        else
+        }
+        else {
           num = num + 36;
+        }
       }
 
       $('body').prepend('<div id="garland"></div>');
@@ -31,14 +33,16 @@
 
       // if admin_menu exists
       if ($('body').hasClass('admin-menu')) {
-        setTimeout(function(){
+        setTimeout(function () {
           var adminMenuHeight = $('#admin-menu').height();
           $('#garland').css('top', adminMenuHeight + 'px');
           $('#garland').css('zIndex', '998');
         }, 500);
       }
 
-      setInterval(function(){ garland() }, 500);
+      setInterval(function () {
+        garland ()
+      }, 500);
 
     }
   };
